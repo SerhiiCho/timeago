8ceb87d0eaf6079577ce6fb9f2e76380ebbe6789
SerhiiCho <serhiicho@protonmail.com>
2019-10-24 22:51:29 +0300

    Refactored function in config_test.go

--- config_test.go
+++ config_test.go
@@ -29,36 +29,44 @@ func TestTrans(t *testing.T) {
 	}
 }
 
-func TestSet(t *testing.T) {
-	t.Run("sets language to ru", func(test *testing.T) {
-		Set("language", "ru")
+func TestSet_for_language(t *testing.T) {
+	cases := []struct {
+		name  string
+		value string
+		err   string
+	}{
+		{"sets language to ru", "ru", "Set must set the `language` variable to `ru` but it didn't"},
+		{"sets language to en", "en", "Set must set the `language` variable to `en` but it didn't"},
+	}
 
-		if language != "ru" {
-			test.Error("Set must set the `language` variable to `ru` but it didn't")
-		}
-	})
+	for _, tc := range cases {
+		t.Run(tc.name, func(test *testing.T) {
+			Set("language", tc.value)
 
-	t.Run("sets language to en", func(test *testing.T) {
-		Set("language", "en")
+			if language != tc.value {
+				test.Error(tc.err)
+			}
+		})
+	}
+}
 
-		if language != "en" {
-			test.Error("Set must set the `language` variable to `en` but it didn't")
-		}
-	})
+func TestSet_for_location(t *testing.T) {
+	cases := []struct {
+		name  string
+		value string
+		err   string
+	}{
+		{"sets location to India Delhi", "India/Delhi", "Set must set the `location` variable to `India/Delhi` but it didn't"},
+		{"sets language to Europe/Kiev", "Europe/Kiev", "Set must set the `location` variable to `Europe/Kiev` but it didn't"},
+	}
 
-	t.Run("sets location to India Delhi", func(test *testing.T) {
-		Set("location", "India/Delhi")
+	for _, tc := range cases {
+		t.Run(tc.name, func(test *testing.T) {
+			Set("location", tc.value)
 
-		if location != "India/Delhi" {
-			test.Error("Set must set the `location` variable to `India/Delhi` but it didn't")
-		}
-	})
-
-	t.Run("sets language to Asia/China", func(test *testing.T) {
-		Set("location", "Europe/Kiev")
-
-		if location != "Europe/Kiev" {
-			test.Error("Set must set the `location` variable to `Europe/Kiev` but it didn't")
-		}
-	})
+			if location != tc.value {
+				test.Error(tc.err)
+			}
+		})
+	}
 }
