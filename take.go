193f34ce131f1d1523cc773af62f60dba4919c81
SerhiiCho <serhiicho@protonmail.com>
2019-10-24 12:08:34 +0300

    Added functions to take.go

--- take.go
+++ take.go
@@ -17,7 +17,57 @@ func Take(datetime string) string {
 	return ""
 }
 
-func hasOption(datetime string) (string, bool) {
+func getLastNumber(num int) int {
+	numStr := strconv.Itoa(num)
+	result, err := strconv.Atoi(numStr[len(numStr)-1:])
+
+	if err != nil {
+		return 0
+	}
+
+	return result
+}
+
+func getWords(typeOf string, num int) string {
+	lastNum := getLastNumber(num)
+	var index int
+
+	switch {
+	case lastNum == 1 && num == 11:
+		index = 2
+		break
+	case lastNum == 1:
+		index = 0
+		break
+	case lastNum > 1 && lastNum < 5:
+		index = 1
+		break
+	default:
+		index = 2
+	}
+
+	timeTrans := getTimeTranslations()
+
+	return strconv.Itoa(num) + " " + timeTrans[typeOf][index] + " " + trans("ago")
+}
+
+// getTimeTranslations returns array of translations for different
+// cases. For example `1 second` must not have `s` at the end
+// but `2 seconds` requires `s`. So this method keeps all
+// possible options for the translated word.
+func getTimeTranslations() map[string][]string {
+	return map[string][]string{
+		"seconds": {trans("second"), trans("seconds"), trans("seconds2")},
+		"minutes": {trans("minute"), trans("minutes"), trans("minutes2")},
+		"hours":   {trans("hour"), trans("hours"), trans("hours2")},
+		"days":    {trans("day"), trans("days"), trans("days2")},
+		"weeks":   {trans("week"), trans("weeks"), trans("weeks2")},
+		"months":  {trans("month"), trans("months"), trans("months2")},
+		"years":   {trans("year"), trans("years"), trans("years2")},
+	}
+}
+
+func getOption(datetime string) (string, bool) {
 	spittedDateString := strings.Split(datetime, "|")
 
 	if len(spittedDateString) > 1 {
