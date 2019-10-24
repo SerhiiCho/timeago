9ac0b024f28e7f0c1c312bdd1e8376ab4017794f
SerhiiCho <serhiicho@protonmail.com>
2019-10-24 08:41:14 +0300

    Added ru.go and en.go files with translations

---
+++ en.go
@@ -0,0 +1,36 @@
+package ago
+
+func getEnglish() map[string]string {
+	return map[string]string{
+		"ago":    "ago ",
+		"online": "Online",
+		// Seconds
+		"second":   "second",
+		"seconds":  "seconds",
+		"seconds2": "seconds",
+		// Minutes
+		"minute":   "minute",
+		"minutes":  "minutes",
+		"minutes2": "minutes",
+		// Hours
+		"hour":   "hour",
+		"hours":  "hours",
+		"hours2": "hours",
+		// Days
+		"day":   "day",
+		"days":  "days",
+		"days2": "days",
+		// Weeks
+		"week":   "week",
+		"weeks":  "weeks",
+		"weeks2": "weeks",
+		// Months
+		"month":   "month",
+		"months":  "months",
+		"months2": "months",
+		// Years
+		"year":   "year",
+		"years":  "years",
+		"years2": "years",
+	}
+}
