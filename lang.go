eee59ac299eb5cb4365ca4d506003ecfa8b0046e
SerhiiCho <serhiicho@protonmail.com>
2019-10-24 12:09:11 +0300

    Added 2 functions to lang.go

--- lang.go
+++ lang.go
@@ -1,5 +1,25 @@
 package timeago
 
-func Lang() {
-	//
+var (
+	translations map[string]string
+	lang         string
+)
+
+func SetLang(langParam string) {
+	lang = langParam
+}
+
+// if translation not found returns null
+func trans(index string) string {
+	return translations[index]
+}
+
+// includeTranslations Includes array of translations
+// from lang directory nto the translations variable.
+func includeTranslations() {
+	if lang == "ru" {
+		translations = getRussian()
+	} else {
+		translations = getEnglish()
+	}
 }
