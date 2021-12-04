27b1d893d984fcff93a6cff1f7dad8d22ab3610f
SerhiiCho <serhiicho@protonmail.com>
2021-12-04 21:38:06 +0200

    Added lang.go

---
+++ lang.go
@@ -0,0 +1,72 @@
+package timeago
+
+import "log"
+
+// getTimeTranslations returns array of translations for different
+// cases. For example `1 second` must not have `s` at the end
+// but `2 seconds` requires `s`. So this method keeps all
+// possible options for the translated word.
+func getTimeTranslations() map[string]map[string]string {
+	return map[string]map[string]string{
+		"seconds": {
+			"single":  trans().Second,
+			"plural":  trans().Seconds,
+			"cpecial": trans().SecondsSpecial,
+		},
+		"minutes": {
+			"single":  trans().Minute,
+			"plural":  trans().Minutes,
+			"cpecial": trans().MinutesSpecial,
+		},
+		"hours": {
+			"single":  trans().Hour,
+			"plural":  trans().Hours,
+			"cpecial": trans().HoursSpecial,
+		},
+		"days": {
+			"single":  trans().Day,
+			"plural":  trans().Days,
+			"cpecial": trans().DaysSpecial,
+		},
+		"weeks": {
+			"single":  trans().Week,
+			"plural":  trans().Weeks,
+			"cpecial": trans().WeeksSpecial,
+		},
+		"months": {
+			"single":  trans().Month,
+			"plural":  trans().Months,
+			"cpecial": trans().MonthsSpecial,
+		},
+		"years": {
+			"single":  trans().Year,
+			"plural":  trans().Years,
+			"cpecial": trans().YearsSpecial,
+		},
+	}
+}
+
+func getLanguageForm(num int) string {
+	var form string
+	lastDigit := getLastNumber(num)
+	rule := getRules(num, lastDigit)[language]
+
+	switch {
+	case rule.Single:
+		return "single"
+	case rule.Plural:
+		return "plural"
+	}
+
+	if rule.Special != nil {
+		for _, isPassing := range rule.Special {
+			if isPassing {
+				return "special"
+			}
+		}
+	}
+
+	log.Fatalf("Provided rules don't apply to a number %d", num)
+
+	return form
+}
--- models/lang.go
+++ models/lang.go
@@ -1,27 +1,27 @@
 package models
 
 type Lang struct {
-	Ago      string
-	Online   string
-	Second   string
-	Seconds  string
-	Seconds2 string
-	Minute   string
-	Minutes  string
-	Minutes2 string
-	Hour     string
-	Hours    string
-	Hours2   string
-	Day      string
-	Days     string
-	Days2    string
-	Week     string
-	Weeks    string
-	Weeks2   string
-	Month    string
-	Months   string
-	Months2  string
-	Year     string
-	Years    string
-	Years2   string
+	Ago            string
+	Online         string
+	Second         string
+	Seconds        string
+	SecondsSpecial string
+	Minute         string
+	Minutes        string
+	MinutesSpecial string
+	Hour           string
+	Hours          string
+	HoursSpecial   string
+	Day            string
+	Days           string
+	DaysSpecial    string
+	Week           string
+	Weeks          string
+	WeeksSpecial   string
+	Month          string
+	Months         string
+	MonthsSpecial  string
+	Year           string
+	Years          string
+	YearsSpecial   string
 }
