1995230487e307f03202ff0f8cbc54611c5f31c3
SerhiiCho <serhiicho@protonmail.com>
2021-12-04 21:40:37 +0200

    Removed redundant code from rules.go

--- rules.go
+++ rules.go
@@ -4,11 +4,11 @@ import "github.com/SerhiiCho/timeago/models"
 
 func getRules(number, lastDigit int) map[string]models.Rule {
 	return map[string]models.Rule{
-		"en": models.Rule{
+		"en": {
 			Single: number == 0 || number == 1,
 			Plural: number > 1,
 		},
-		"ru": models.Rule{
+		"ru": {
 			Single: lastDigit == 1 || number == 0,
 			Plural: lastDigit >= 2 && lastDigit < 5,
 			Special: []bool{
