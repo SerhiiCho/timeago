717f9579fffaed1a574cf73a115e4fbdb5de924b
SerhiiCho <serhiicho@protonmail.com>
2022-01-22 11:13:01 +0200

    Added tests to fileExists() function

--- utils_test.go
+++ utils_test.go
@@ -13,3 +13,21 @@ func TestParseJsonIntoLang(t *testing.T) {
 		}
 	})
 }
+
+func TestFileExists(t *testing.T) {
+	t.Run("fileExists return false if file doesn't exist", func(test *testing.T) {
+		result, _ := fileExists("somerandompath")
+
+		if result {
+			t.Errorf("Function fileExists must return false, because filepath points to a file that doesn't exist")
+		}
+	})
+
+	t.Run("fileExists return true if file exist", func(test *testing.T) {
+		result, _ := fileExists("timeago.go")
+
+		if result == false {
+			t.Errorf("Function fileExists must return true, because filepath points to a file that exists")
+		}
+	})
+}
