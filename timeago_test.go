3a04deeaf3d95927f754ff6e2fe8b5d3185bfb88
SerhiiCho <serhiicho@protonmail.com>
2022-01-22 11:54:44 +0200

    Moved 1 test from timeago_test to utils_test

--- timeago_test.go
+++ timeago_test.go
@@ -40,31 +40,6 @@ func TestGetWords(t *testing.T) {
 	}
 }
 
-func TestGetLastNumberDigit(t *testing.T) {
-	cases := []struct {
-		number int
-		result int
-		name   string
-	}{
-		{0, 0, "must return 0"},
-		{1, 1, "must return 1"},
-		{9, 9, "must return 9"},
-		{20, 0, "must return 0"},
-		{253, 3, "must return 3"},
-		{23423252, 2, "must return 2"},
-		{223424342325, 5, "must return 5"},
-		{23423521562348, 8, "must return 8"},
-	}
-
-	for _, tc := range cases {
-		t.Run(tc.name, func(test *testing.T) {
-			if res := getLastNumberDigit(tc.number); res != tc.result {
-				test.Errorf("Result must be %d, but got %d instead", tc.result, res)
-			}
-		})
-	}
-}
-
 func TestParseFunctionCanExceptTimestamp(t *testing.T) {
 	cases := []struct {
 		timestamp int
