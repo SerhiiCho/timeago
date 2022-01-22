9f31a8d08f3b67d5286bae919a0ff01582450aa4
SerhiiCho <serhiicho@protonmail.com>
2022-01-22 15:03:30 +0200

    Refactored ru_test.go

--- tests/ru_test.go
+++ tests/ru_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestParseRu(t *testing.T) {
 	cases := []struct {
-		date   string
+		date   time.Time
 		result string
 	}{
 		{subMinutes(1), "1 минута назад"},
@@ -53,7 +53,7 @@ func TestParseRu(t *testing.T) {
 	}
 
 	for _, tc := range cases {
-		t.Run("result for "+tc.date, func(test *testing.T) {
+		t.Run("result for "+tc.date.String(), func(test *testing.T) {
 			SetConfig(Config{
 				Language: "ru",
 			})
@@ -67,65 +67,62 @@ func TestParseRu(t *testing.T) {
 
 func TestParseRuWithOnlineFlag(t *testing.T) {
 	cases := []struct {
-		date   string
+		date   time.Time
 		result string
 	}{
-		{smallSubTime(time.Second * 2), "В сети"},
-		{smallSubTime(time.Second), "В сети"},
-		{smallSubTime(-1 * time.Second), "В сети"},
-		{smallSubTime(-2 * time.Second), "В сети"},
-		{smallSubTime(-9 * time.Second), "В сети"},
-		{smallSubTime(-10 * time.Second), "В сети"},
-		{smallSubTime(-11 * time.Second), "В сети"},
-		{smallSubTime(-20 * time.Second), "В сети"},
-		{smallSubTime(-21 * time.Second), "В сети"},
-		{smallSubTime(-22 * time.Second), "В сети"},
-		{smallSubTime(-30 * time.Second), "В сети"},
-		{smallSubTime(-31 * time.Second), "В сети"},
-		{smallSubTime(-60 * time.Second), "1 минута назад"},
-		{smallSubTime(-1 * time.Minute), "1 минута назад"},
-		{smallSubTime(-2 * time.Minute), "2 минуты назад"},
-		{smallSubTime(-9 * time.Minute), "9 минут назад"},
-		{smallSubTime(-10 * time.Minute), "10 минут назад"},
-		{smallSubTime(-11 * time.Minute), "11 минут назад"},
-		{smallSubTime(-20 * time.Minute), "20 минут назад"},
-		{smallSubTime(-21 * time.Minute), "21 минута назад"},
-		{smallSubTime(-22 * time.Minute), "22 минуты назад"},
-		{smallSubTime(-30 * time.Minute), "30 минут назад"},
-		{smallSubTime(-31 * time.Minute), "31 минута назад"},
-		{smallSubTime(-60 * time.Minute), "1 час назад"},
-		{smallSubTime(-1 * time.Hour), "1 час назад"},
-		{smallSubTime(-2 * time.Hour), "2 часа назад"},
-		{smallSubTime(-9 * time.Hour), "9 часов назад"},
-		{smallSubTime(-10 * time.Hour), "10 часов назад"},
-		{smallSubTime(-11 * time.Hour), "11 часов назад"},
-		{smallSubTime(-20 * time.Hour), "20 часов назад"},
-		{smallSubTime(-21 * time.Hour), "21 час назад"},
-		{smallSubTime(-23 * time.Hour), "23 часа назад"},
-		{smallSubTime(-24 * time.Hour), "1 день назад"},
-		{smallSubTime(-30 * time.Hour), "1 день назад"},
-		{smallSubTime((-24 * 2) * time.Hour), "2 дня назад"},
-		{smallSubTime((-24 * 6) * time.Hour), "6 дней назад"},
-		{smallSubTime((-24 * 7) * time.Hour), "1 неделя назад"},
-		{smallSubTime((-24 * 14) * time.Hour), "2 недели назад"},
-		{smallSubTime((-24 * 21) * time.Hour), "3 недели назад"},
-		{bigSubTime(0, 1, 1), "1 месяц назад"},
-		{bigSubTime(0, 2, 1), "2 месяца назад"},
-		{bigSubTime(0, 9, 1), "9 месяцев назад"},
-		{bigSubTime(0, 11, 1), "11 месяцев назад"},
-		{bigSubTime(0, 12, 1), "1 год назад"},
-		{bigSubTime(1, 0, 1), "1 год назад"},
-		{bigSubTime(2, 0, 1), "2 года назад"},
-		{bigSubTime(5, 0, 1), "5 лет назад"},
-		{bigSubTime(6, 0, 1), "6 лет назад"},
-		{bigSubTime(7, 0, 1), "7 лет назад"},
-		{bigSubTime(21, 0, 1), "21 год назад"},
-		{bigSubTime(31, 0, 1), "31 год назад"},
-		{bigSubTime(100, 0, 1), "100 лет назад"},
+		{subSeconds(1), "В сети"},
+		{subSeconds(2), "В сети"},
+		{subSeconds(1), "В сети"},
+		{subSeconds(2), "В сети"},
+		{subSeconds(9), "В сети"},
+		{subSeconds(10), "В сети"},
+		{subSeconds(11), "В сети"},
+		{subSeconds(20), "В сети"},
+		{subSeconds(21), "В сети"},
+		{subSeconds(20), "В сети"},
+		{subSeconds(30), "В сети"},
+		{subSeconds(31), "В сети"},
+		{subSeconds(60), "1 минута назад"},
+		{subMinutes(1), "1 минута назад"},
+		{subMinutes(2), "2 минуты назад"},
+		{subMinutes(9), "9 минут назад"},
+		{subMinutes(10), "10 минут назад"},
+		{subMinutes(11), "11 минут назад"},
+		{subMinutes(20), "20 минут назад"},
+		{subMinutes(21), "21 минута назад"},
+		{subMinutes(22), "22 минуты назад"},
+		{subMinutes(30), "30 минут назад"},
+		{subMinutes(31), "31 минута назад"},
+		{subHours(1), "1 час назад"},
+		{subHours(2), "2 часа назад"},
+		{subHours(9), "9 часов назад"},
+		{subHours(10), "10 часов назад"},
+		{subHours(11), "11 часов назад"},
+		{subHours(20), "20 часов назад"},
+		{subHours(21), "21 час назад"},
+		{subHours(23), "23 часа назад"},
+		{subDays(1), "1 день назад"},
+		{subDays(2), "2 дня назад"},
+		{subDays(6), "6 дней назад"},
+		{subWeeks(1), "1 неделя назад"},
+		{subWeeks(2), "2 недели назад"},
+		{subWeeks(3), "3 недели назад"},
+		{subMonths(1), "1 месяц назад"},
+		{subMonths(2), "2 месяца назад"},
+		{subMonths(9), "9 месяцев назад"},
+		{subMonths(11), "11 месяцев назад"},
+		{subYears(1), "1 год назад"},
+		{subYears(2), "2 года назад"},
+		{subYears(5), "5 лет назад"},
+		{subYears(6), "6 лет назад"},
+		{subYears(7), "7 лет назад"},
+		{subYears(21), "21 год назад"},
+		{subYears(31), "31 год назад"},
+		{subYears(100), "100 лет назад"},
 	}
 
 	for _, tc := range cases {
-		t.Run("result for "+tc.date, func(test *testing.T) {
+		t.Run("result for "+tc.date.String(), func(test *testing.T) {
 
 			SetConfig(Config{
 				Language: "ru",
