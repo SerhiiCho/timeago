5c74e390d6ff6d984b31ef6096e1ee8c67efda1b
SerhiiCho <serhiicho@protonmail.com>
2022-01-22 15:03:25 +0200

    Changed utils.go

--- tests/utils.go
+++ tests/utils.go
@@ -6,45 +6,54 @@ import (
 	"github.com/SerhiiCho/timeago"
 )
 
-var (
+const (
 	second time.Duration = time.Second
 	minute time.Duration = time.Minute
 	hour   time.Duration = time.Hour
 	day    time.Duration = hour * 24
 	week   time.Duration = day * 7
+	// we cannot add month and year because months and years cannot have static values
 )
 
-func subTime(duration time.Duration) string {
-	return time.Now().Add(-duration).Format("2006-01-02 15:04:05")
+func subTime(duration time.Duration) time.Time {
+	return time.Now().Add(-duration)
 }
 
-func subMinutes(duration time.Duration) string {
+func subSeconds(duration time.Duration) time.Time {
+	return subTime(second * duration)
+}
+
+func subMinutes(duration time.Duration) time.Time {
 	return subTime(minute * duration)
 }
 
-func subHours(duration time.Duration) string {
+func subHours(duration time.Duration) time.Time {
 	return subTime(hour * duration)
 }
 
-func subDays(duration time.Duration) string {
+func subDays(duration time.Duration) time.Time {
 	return subTime(day * duration)
 }
 
-func subWeeks(duration time.Duration) string {
+func subWeeks(duration time.Duration) time.Time {
 	return subTime(week * duration)
 }
 
-func subMonths(duration time.Duration) string {
-	daysInMonth := getLastDayOfMonth(time.Now()).Day()
-	result := duration * time.Duration(daysInMonth) * day
-	return subTime(result)
+func subMonths(duration time.Duration) time.Time {
+	return subTime(duration * getDaysInMonth() * day)
 }
 
-func subYears(duration time.Duration) string {
-	lastDayOfTheYear := time.Date(time.Now().Year(), 12, 31, 0, 0, 0, 0, time.UTC)
-	daysInYear := time.Duration(lastDayOfTheYear.YearDay())
+func subYears(duration time.Duration) time.Time {
+	return subTime(duration * getDaysInYear() * day)
+}
 
-	return subTime(duration * daysInYear * day)
+func getDaysInYear() time.Duration {
+	lastDayOfTheYear := time.Date(time.Now().Year(), 12, 31, 0, 0, 0, 0, time.UTC)
+	return time.Duration(lastDayOfTheYear.YearDay())
+}
+
+func getDaysInMonth() time.Duration {
+	return time.Duration(getLastDayOfMonth(time.Now()).Day())
 }
 
 func getLastDayOfMonth(date time.Time) time.Time {
