4fda89ca46168fd905a672e6753c54ecca8c2580
SerhiiCho <serhiicho@protonmail.com>
2024-11-12 11:38:21 +0200

    WIP

--- langset/langset.go
+++ langset/langset.go
@@ -3,26 +3,26 @@ package langset
 import "github.com/SerhiiCho/timeago/v3/ctx"
 
 type CLDR struct {
-	One   string
-	Few   string
-	Many  string
-	Other string
+	One   string `json:"one"`
+	Few   string `json:"few"`
+	Many  string `json:"many"`
+	Other string `json:"other"`
 }
 
 type LangSet struct {
-	Ago     string
-	Online  string
-	JustNow string
-	Second  CLDR
-	Minute  CLDR
-	Hour    CLDR
-	Day     CLDR
-	Week    CLDR
-	Month   CLDR
-	Year    CLDR
+	Ago     string `json:"ago"`
+	Online  string `json:"online"`
+	JustNow string `json:"justnow"`
+	Second  CLDR   `json:"second"`
+	Minute  CLDR   `json:"minute"`
+	Hour    CLDR   `json:"hour"`
+	Day     CLDR   `json:"day"`
+	Week    CLDR   `json:"week"`
+	Month   CLDR   `json:"month"`
+	Year    CLDR   `json:"year"`
 }
 
 func New(c *ctx.Ctx) LangSet {
 	lang := c.Config().Language
-	return parseJsonIntoTrans("langset/" + lang + ".json")
+	return parseJsonIntoTrans("langs/" + lang + ".json")
 }
