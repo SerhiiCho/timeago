773ce5bdbc81bd2d9a6166fd50672e27f70cd2fd
SerhiiCho <serhiicho@protonmail.com>
2024-11-14 16:24:21 +0200

    Remove "other" rule from rules.go file

--- rules.go
+++ rules.go
@@ -1,12 +1,11 @@
 package timeago
 
 type rule struct {
-	Zero  bool
-	One   bool
-	Two   bool
-	Few   bool
-	Many  bool
-	Other bool
+	Zero bool
+	One  bool
+	Two  bool
+	Few  bool
+	Many bool
 }
 
 func identifyGrammarRules(num int) map[string]rule {
@@ -14,12 +13,11 @@ func identifyGrammarRules(num int) map[string]rule {
 
 	return map[string]rule{
 		"en": {
-			Zero:  num == 0,
-			One:   num == 1,
-			Two:   num == 2,
-			Few:   num > 1,
-			Many:  num > 1,
-			Other: num > 1,
+			Zero: num == 0,
+			One:  num == 1,
+			Two:  num == 2,
+			Few:  num > 1,
+			Many: num > 1,
 		},
 		"ru": {
 			Zero: num == 0,
@@ -29,28 +27,25 @@ func identifyGrammarRules(num int) map[string]rule {
 			Many: (num >= 5 && num <= 20) || lastDigit == 0 || (lastDigit >= 5 && lastDigit <= 9),
 		},
 		"uk": {
-			Zero:  num == 0,
-			One:   lastDigit == 1,
-			Two:   num == 2,
-			Few:   num == 2,
-			Many:  lastDigit >= 2 && lastDigit < 5,
-			Other: (num >= 5 && num <= 20) || lastDigit == 0 || (lastDigit >= 5 && lastDigit <= 9),
+			Zero: num == 0,
+			One:  lastDigit == 1,
+			Two:  num == 2,
+			Few:  num == 2,
+			Many: lastDigit >= 2 && lastDigit < 5,
 		},
 		"nl": {
-			Zero:  num == 0,
-			One:   num == 1,
-			Few:   num > 1,
-			Two:   num == 2,
-			Many:  num > 1,
-			Other: num > 1,
+			Zero: num == 0,
+			One:  num == 1,
+			Few:  num > 1,
+			Two:  num == 2,
+			Many: num > 1,
 		},
 		"de": {
-			Zero:  num == 0,
-			One:   num == 1,
-			Few:   num > 1,
-			Two:   num == 2,
-			Many:  num > 1,
-			Other: num > 1,
+			Zero: num == 0,
+			One:  num == 1,
+			Few:  num > 1,
+			Two:  num == 2,
+			Many: num > 1,
 		},
 	}
 }
