1f57ca28ddbb0906b9e3ebd8312b5430fe4031dd
SerhiiCho <serhiicho@protonmail.com>
2025-06-16 12:05:54 +0300

    refactor: rename test

--- utils_test.go
+++ utils_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestParseJsonIntoLang(t *testing.T) {
+func Test_parseJsonIntoLang(t *testing.T) {
 	t.Run("Function returns Lang model with needed values", func(test *testing.T) {
 		res, err := parseLangSet("langs/ru.json")
 
